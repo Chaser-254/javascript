643908fb60821456e53c4788edc97fea86e3b198
Chaser-254 <81488755+Chaser-254@users.noreply.github.com>
2022-02-18 09:46:47 +0300

    grading system using java

---
+++ grading.java
@@ -0,0 +1,26 @@
+class IfElseDemo {
+    public static void main(string[ args[]) {
+        int testscores = 776;
+        char grade;
+        
+        if(testscores >= 90) {
+            grade = 'A';
+        else if (testscores >= 80) {
+            grade = 'B';
+
+        else if (testscores >= 70) {
+            grade = 'C';
+
+        else if (testscores >= 60) {
+            grade = 'C';
+
+        else {
+            grade = 'F'
+        }
+        }
+        }
+        }
+        }
+        system.out.println("Grade = " + grade);
+    }
+}
